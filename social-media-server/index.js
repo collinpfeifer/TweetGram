ad15d5c7bb427645b2b9c10af1c60b7923d905d6
Collin Pfeifer <collinpfeifer47@collins-mbp.lan>
2021-02-24 15:13:43 -0500

    server side initial commit
    initilazed server with apollo and other dependencies

---
+++ social-media-server/index.js
@@ -0,0 +1,20 @@
+const { ApolloServer } = require('apollo-server');
+const gql = require('graphql-tag');
+const typeDefs = gql`
+    type Query {
+        sayHi: String!
+    }
+`
+
+const resolvers = {
+    Query: {
+        sayHi: () => 'hello world'
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+server.listen({port: 5000})
